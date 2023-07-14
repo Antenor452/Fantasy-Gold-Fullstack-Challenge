e7260a8b589c4410757b4e51dd14a3389125c205
Antenor452 <nanaosei452@gmail.com>
2023-07-14 21:39:27 +0000

    initial commit

--- fantasygold-fullstack-challenge/src/index.js
+++ fantasygold-fullstack-challenge/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import routes from "../src/routes";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <RouterProvider router={routes}>
+        <App />
+      </RouterProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
